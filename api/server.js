dfc5d1d6cddfa2ec8ea183e63d82327630813f7d
NeRo707 <tazo.gorgadze.02@gmail.com>
2023-09-12 21:30:37 +0400

    racxa

--- api/server.js
+++ api/server.js
@@ -5,11 +5,16 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
-
+import cors from 'cors';
 
 const port = process.env.PORT || 5000;
 const app = express();
 
+const corsOptions = {
+  origin: 'https://nero-mern-app.onrender.com',
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
