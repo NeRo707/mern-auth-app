d4cd472a4a56484994ac252651267de332a66e11
NeRo <99098290+NeRo707@users.noreply.github.com>
2023-09-17 00:29:21 +0400

    Update userRoutes.js

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -14,6 +14,6 @@ import { protect } from "../middleware/authMiddleware.js";
 router.post("/", registerUser);
 router.post("/auth", authUser);
 router.post("/logout", logoutUser);
-router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile);
+router.route("/profile").get(protect, getUserProfile).put(updateUserProfile);
 
 export default router;
