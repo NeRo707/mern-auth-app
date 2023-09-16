ad892fa0bebe406947544b494b7c2cc1fd2d3205
NeRo <99098290+NeRo707@users.noreply.github.com>
2023-09-16 23:16:10 +0400

    Update server.js

--- api/server.js
+++ api/server.js
@@ -16,9 +16,9 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
