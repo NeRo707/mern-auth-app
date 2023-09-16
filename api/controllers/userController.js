df0464f4d334562bcdbc0a72f11ad9d5a103217a
NeRo <99098290+NeRo707@users.noreply.github.com>
2023-09-16 23:13:10 +0400

    Update userController.js

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -93,7 +93,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
 */
 const updateUserProfile = asyncHandler(async (req, res) => {
 
-  const user = await User.findById(req.user._id);
+  const user = await User.findById(req.body._id);
 
   if(user){
     user.name = req.body.name || user.name;
