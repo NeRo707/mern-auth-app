f435c5add0ddd4765b2b36632fe3b04b201d093e
NeRo <99098290+NeRo707@users.noreply.github.com>
2023-09-16 23:43:04 +0400

    Update usersApiSlice.js

--- client/src/slices/usersApiSlice.js
+++ client/src/slices/usersApiSlice.js
@@ -9,6 +9,7 @@ export const userApiSlice = apiSlice.injectEndpoints({
         url: `${USERS_URL}/auth`,
         method: "POST",
         body: data,
+        credentials: "include",
       }),
     }),
     register: builder.mutation({
@@ -16,12 +17,14 @@ export const userApiSlice = apiSlice.injectEndpoints({
         url: `${USERS_URL}`,
         method: "POST",
         body: data,
+        credentials: "include",
       }),
     }),
     logout: builder.mutation({
       query: () => ({
         url: `${USERS_URL}/logout`,
         method: "POST",
+        credentials: "include",
       }),
     }),
     updateUser: builder.mutation({
@@ -29,6 +32,7 @@ export const userApiSlice = apiSlice.injectEndpoints({
         url: `${USERS_URL}/profile`,
         method: "PUT",
         body: data,
+        credentials: "include",
       }),
     })
   }),
