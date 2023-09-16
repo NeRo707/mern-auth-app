3b3898747ddfecbd7cc345056eeb0917d1bd03a2
NeRo <99098290+NeRo707@users.noreply.github.com>
2023-09-17 00:06:51 +0400

    Update generateToken.js

--- api/utils/generateToken.js
+++ api/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateToken = (res, userId) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development',
+    secure: true,
     sameSite: 'strict',
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   })
